218ebdcd3d93ee2f8e4fc90ca3cb59d2f5a7ca1b
Feridun Canselen <gadmer@Feriduns-MacBook-Air.local>
2021-10-23 19:52:03 +0300

    walk test pass

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,12 +1,40 @@
-const Pet = require('../src/pet');
-describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
-    });
+const Pet = require("../src/pet");
+describe("constructor", () => {
+  it("returns an object", () => {
+    expect(new Pet("Fido")).toBeInstanceOf(Object);
   });
-  it('has a initial age of 0', () => {
-    const pet = new Pet('Fido');
+});
+describe("age function", () => {
+  it("has an initial age of 0", () => {
+    const pet = new Pet("Fido");
     pet.growUp();
 
     expect(pet.age).toEqual(1);
   });
+});
+describe("hunger level", () => {
+  it("has an initial hunger of 0", () => {
+    const pet = new Pet("Fido");
+    const rex = new Pet("Rex");
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+});
+describe("fitness level", () => {
+  it("has an initial fitness level of 10", () => {
+    const pet = new Pet("Fido");
+    const rex = new Pet("Rex");
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  });
+});
+describe('walk', () => {
+    it('increases fitness by to a maximum of 10', () => {
+        const pet = new Pet('fido');
+    
+        pet.fitness = 8;
+        pet.walk();
+    
+        expect(pet.fitness).toEqual(10);
+      });
+    });
