b88d565c366509bc4e07619623bee35fcd6acf35
Feridun Canselen <gadmer@Feriduns-MacBook-Air.local>
2021-10-25 14:02:37 +0300

    finished checkup

--- src/pet.js
+++ src/pet.js
@@ -1,4 +1,5 @@
 const MAXIMUM_FITNESS = 10;
+const MINUMUM_HUNGER = 0;
 function Pet(name) {
     this.name = name;
     this.age = 0;
@@ -16,6 +17,18 @@ function Pet(name) {
           } else {
             this.fitness = 10;
           }
+      }
+      Pet.prototype.checkUp = function() {
+        if (this.fitness <= 3 && this.hunger >= 5) {
+            return 'I am hungry AND I need a walk'
+            }
+          if (this.fitness <= 3) {
+              return 'I need a walk';
+          }
+          if (this.hunger >= 5) {
+              return 'I am hungry';
+          }
+          else return "I feel great!";
       }
 };
 const pet = new Pet("Fido");
